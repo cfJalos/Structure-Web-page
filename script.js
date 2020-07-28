4bf985d6ee494a3e2f7085fcbe46882c1b351bd9
Czarl Jalos <68618234+cfJalos@users.noreply.github.com>
2020-07-27 21:26:07 -0700

    Add greetings javascript function.

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ var lastName = prompt('What is your last name?');
 var age = prompt('What is your age?');
 
 console.log('Your full name is ' + firstName + " " + lastName);
+
 console.log('Your are ' + age + ' ' + 'years old');
-
-
 
 var txt;
 
@@ -15,4 +14,21 @@ if (confirm("Proceed to website?")) {
     txt = "Hope to see you again!";
   }
 
-  alert(txt);
+alert(txt);
+
+function greeting() {
+
+var today = new Date()
+var curHr = today.getHours()
+var greetings;
+
+  if (curHr < 12) {
+    greetings = 'Good morning!';
+  } else if (curHr < 18) {
+    greetings = 'Good afternoon!';
+  } else {
+    greetings = 'Good evening!';
+}
+
+document.write('<h3>' + greetings + '</h3>');
+}
