b783b878ef24c574e4378645d2ef9f20d0fe8a5d
Czarl Jalos <68618234+cfJalos@users.noreply.github.com>
2020-07-27 18:11:21 -0700

    Create javascript file for prompt, console log, and confirm.

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18) {
-    greeting = 'Good evening!';
-} else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-} else if (hourNow > 0) {
-    greeting = 'Good morning!';
-}else {
-    greeting = 'Welcome!';
-}
-
-document.write('<h3>' + greeting + '</h3>');
-
 var firstName = prompt('What is your first name?');
 var lastName = prompt('What is your last name?');
 var age = prompt('What is your age?');
 
 console.log('Your full name is ' + firstName + " " + lastName);
-console.log('Your are ' + age + ' ' years old);
+console.log('Your are ' + age + ' ' + 'years old');
+
+
+
+var txt;
+
+if (confirm("Proceed to website?")) {
+    txt = "Enjoy!";
+  } else {
+    txt = "Hope to see you again!";
+  }
+
+  alert(txt);
